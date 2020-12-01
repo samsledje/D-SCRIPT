793035359b6d45c0206181ba857b75f611c34cb7
Samuel Sledzieski <samsledje@gmail.com>
2020-11-30 21:42:50 -0500

    added dev branch

--- dscript/__init__.py
+++ dscript/__init__.py
@@ -1,2 +1,2 @@
-__version__ = "0.1.2"
+__version__ = "0.2.0-dev"
 __citation__ = "Sledzieski, Singh, Cowen, Berger. Sequence-based prediction of protein-protein interactions: a structure-aware interpretable deep learning model. Under Review. 2020."
