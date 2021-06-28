9dfe2c9f9ee69ffac19dd18fa5e27f534ec49f30
Jesse Yang <jesseyang432@gmail.com>
2021-06-28 14:03:38 -0500

    Added Home page and Router

--- web-server/frontend/src/App.js
+++ web-server/frontend/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './components/pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route path='/' exact component={Home}/>
+      </div>
+    </Router>
   );
 }
 
