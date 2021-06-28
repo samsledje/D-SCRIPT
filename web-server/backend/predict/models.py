c3c01055eb9aa29606e4d44763f255afdc68e411
Jesse Yang <jesseyang432@gmail.com>
2021-06-28 13:17:08 -0500

    Added Prediction model and created migration file

--- web-server/backend/predict/models.py
+++ web-server/backend/predict/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Prediction(models.Model):
+    title = models.CharField(max_length=120)
+    protein1 = models.CharField(max_length=60)
+    protein2 = models.CharField(max_length=60)
+    sequence1 = models.TextField()
+    sequence2 = models.TextField()
+    probability = models.IntegerField()
+
+    def __str__(self):
+        return self.title
