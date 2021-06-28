2331c49f4c680598ccefdc3c4fca4b41dd120536
Jesse Yang <jesseyang432@gmail.com>
2021-06-28 13:28:00 -0500

    Added Django admin interface interactability

--- web-server/backend/predict/admin.py
+++ web-server/backend/predict/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Prediction
+
+class PredictionAdmin(admin.ModelAdmin):
+    # Displays within admin panel
+    list_display = ('title', 'protein1', 'protein2', 'sequence1', 'sequence2', 'probability')
 
 # Register your models here.
+
+admin.site.register(Prediction, PredictionAdmin)
