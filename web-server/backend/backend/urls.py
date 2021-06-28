4f3be8cc1cad87e19d8a4f80d2fce0696ea590cb
Jesse Yang <jesseyang432@gmail.com>
2021-06-28 13:43:36 -0500

    Renamed to import from predict

--- web-server/backend/backend/urls.py
+++ web-server/backend/backend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from todo import views
+from predict import views
 
 router = routers.DefaultRouter()
 router.register(r'predictions', views.PredictionView, 'predict')
