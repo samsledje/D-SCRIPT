783b4d159a4e73005100b95a8f09e8c9c4bb9674
Jesse Yang <jesseyang432@gmail.com>
2021-06-28 14:47:18 -0500

    Added further styling and components

--- web-server/frontend/src/components/modules/Predict.js
+++ web-server/frontend/src/components/modules/Predict.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import Protein from './Protein'
+
 export default function Predict() {
     return (
         <div className="Predict-Container">
-            Hi
+            <form>
+                <label>Enter a job title: </label><br></br>
+                <input type="text"></input><br></br>
+            </form>
+            <div className="Predict-Proteins">
+                <Protein></Protein>
+                <Protein></Protein>
+            </div>
         </div>
     )
 }
---
+++ web-server/frontend/src/components/modules/Protein.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Protein() {
+    return (
+        <div className="Protein-Container">
+            <form className="Protein-Box">
+                <label>Protein #1: </label><br></br>
+                <input type="text"></input><br></br>
+                <label>Sequence #1: </label><br></br>
+                <textarea></textarea><br></br>
+            </form>
+        </div>
+    )
+}
