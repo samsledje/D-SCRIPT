be3344e5cd8903ea717b6e03ef6c97133c1476ce
Jesse Yang <jesseyang432@gmail.com>
2021-06-28 13:42:33 -0500

    Added basic backend urls usability

--- web-server/backend/backend/urls.py
+++ web-server/backend/backend/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from todo import views
+
+router = routers.DefaultRouter()
+router.register(r'predictions', views.PredictionView, 'predict')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
