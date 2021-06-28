ceadb104ed57eb7c21fda4dd8dbf496656037b22
Jesse Yang <jesseyang432@gmail.com>
2021-06-28 14:06:06 -0500

    Initialized styles.scss

--- web-server/frontend/src/App.js
+++ web-server/frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home'
+import './styles.css'
 
 function App() {
   return (
