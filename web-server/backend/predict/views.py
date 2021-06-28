5b51db6c4940da6208c895c25ddfd6c188d8d2fe
Jesse Yang <jesseyang432@gmail.com>
2021-06-28 13:40:44 -0500

    Created basic View

--- web-server/backend/predict/views.py
+++ web-server/backend/predict/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_frameowrk import viewsets
+from .serializers import PredictionSerializer
+from .models import Prediction
 
 # Create your views here.
+
+class PredictionView(viewsets.ModelViewSet):
+    serializer_class = PredictionSerializer
+    queryset = Prediction.objects.all()
