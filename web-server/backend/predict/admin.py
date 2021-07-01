fd1b48b4ad2da2cd2d1ae725f17329646fb95561
Jesse Yang <jesseyang432@gmail.com>
2021-07-01 14:34:34 -0500

    Registered FilePrediction model on admin site

--- web-server/backend/predict/admin.py
+++ web-server/backend/predict/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from .models import Prediction
+from .models import FilePrediction
 
 class PredictionAdmin(admin.ModelAdmin):
     # Displays within admin panel
     list_display = ('title', 'protein1', 'protein2', 'sequence1', 'sequence2', 'probability')
 
+class FilePredictionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pairs', 'sequences', 'predictions')
+
 # Register your models here.
 
 admin.site.register(Prediction, PredictionAdmin)
+admin.site.register(FilePrediction, FilePredictionAdmin)
