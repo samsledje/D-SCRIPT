1359820f355d1b65b6d398a87ac27ea91e8c6824
Jesse Yang <jesseyang432@gmail.com>
2021-07-01 14:08:29 -0500

    Completed basic file submission frontend

--- web-server/frontend/src/components/modules/FilePredict.js
+++ web-server/frontend/src/components/modules/FilePredict.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function FilePredict() {
     return (
         <div className="FilePredict-Container">
+            <h1>Predict Interaction Between Protein Pairs</h1>
             <form>
                 <label>Enter a job title:</label><br></br>
                 <input type="text"></input><br></br>
@@ -11,6 +12,7 @@ export default function FilePredict() {
                 <label>Protein sequences (.fasta)</label><br></br>
                 <input type="file" accept=".fasta"></input><br></br>
             </form>
+            <button>Compute Interaction Probability</button>
         </div>
     )
 }
