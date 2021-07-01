804b57a0d801dc46797a9e9f14d7e3a60df46fde
Jesse Yang <jesseyang432@gmail.com>
2021-07-01 14:28:32 -0500

    Handle input changes in FilePredict

--- web-server/frontend/src/components/modules/FilePredict.js
+++ web-server/frontend/src/components/modules/FilePredict.js
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function FilePredict() {
+    const [item, setItem] = useState({
+        'title': '',
+        'pairs': null,
+        'sequences': null
+    });
+
+    const handleTitleChange = (e) => {
+        setItem({...item, 'title': e.target.value});
+    }
+
+    const handlePairsChange = (e) => {
+        setItem({...item, 'pairs': e.target.value});
+    }
+
+    const handleSequencesChange = (e) => {
+        setItem({...item, 'sequences': e.target.value})
+    }
+
     return (
         <div className="FilePredict-Container">
             <h1>Predict Interaction Between Protein Pairs</h1>
             <form>
                 <label>Enter a job title:</label><br></br>
-                <input type="text"></input><br></br>
+                <input type="text" onChange={handleTitleChange}></input><br></br>
                 <label>Protein candidate pairs (.tsv)</label><br></br>
-                <input type="file" accept=".tsv"></input><br></br>
+                <input type="file" accept=".tsv" onChange={handlePairsChange}></input><br></br>
                 <label>Protein sequences (.fasta)</label><br></br>
-                <input type="file" accept=".fasta"></input><br></br>
+                <input type="file" accept=".fasta" onChange={handleSequencesChange}></input><br></br>
             </form>
-            <button>Compute Interaction Probability</button>
+            <button onClick={() => console.log(item)}>Compute Interaction Probability</button>
         </div>
     )
 }
