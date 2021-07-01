00bf752613d84a11ee1b50622d1b268592c4c8e2
Jesse Yang <jesseyang432@gmail.com>
2021-07-01 14:37:01 -0500

    Implemented FilePredictionSerializer

--- web-server/backend/predict/serializers.py
+++ web-server/backend/predict/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Prediction
+from .models import FilePrediction
 
 class PredictionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Prediction
         fields = ('id', 'title', 'protein1', 'protein2', 'sequence1', 'sequence2', 'probability')
+
+class FilePredictionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FilePrediction
+        fiels = ('id', 'title', 'pairs', 'sequences', 'predictions')
