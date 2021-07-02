4b6d94404a805288088d312c571663cac4ced799
Jesse Yang <jesseyang432@gmail.com>
2021-07-02 14:06:58 -0500

    Modified upload urls

--- web-server/backend/predict/models.py
+++ web-server/backend/predict/models.py
@@ -16,8 +16,8 @@ class Prediction(models.Model):
 class FilePrediction(models.Model):
     title = models.CharField(max_length=120)
     pairs = models.FileField(upload_to='pairs/')
-    sequences = models.FileField()
-    predictions = models.FileField()
+    sequences = models.FileField(upload_to='sequences/')
+    predictions = models.FileField(upload_to='predictions/')
 
     def __str__(self):
         return self.title
