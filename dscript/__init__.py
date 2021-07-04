e928480741502f3fc982d3ce8c2f1ddf636aafbb
Samuel Sledzieski <samsledje@gmail.com>
2021-07-04 10:58:14 -0400

    checking pre-commit hooks

--- dscript/__init__.py
+++ dscript/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.0-dev"
+__version__ = "0.2.1-dev"
 __citation__ = "Sledzieski, Singh, Cowen, Berger. Sequence-based prediction of protein-protein interactions: a structure-aware interpretable deep learning model. Under Review. 2020."
 import dscript.models
 import dscript.commands
