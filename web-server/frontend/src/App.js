38b0c4c71a5a8ba01d6d89ccfdfc874f56101ca4
Jesse Yang <jesseyang432@gmail.com>
2021-07-09 14:16:47 -0500

    Added routing for page components

--- web-server/frontend/src/App.js
+++ web-server/frontend/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Home from './components/pages/Home'
-import './styles.css'
+import Home from './components/pages/Home';
+import SinglePairPredict from './components/pages/SinglePairPredict';
+import ManyPairsPredict from './components/pages/ManyPairsPredict';
+import './styles.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route path='/' exact component={Home}/>
+        <Route path='/pair' exact component={SinglePairPredict}/>
+        <Route path='/pairs' exact component={ManyPairsPredict}/>
       </div>
     </Router>
   );
