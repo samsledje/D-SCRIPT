09dbe1245de7097df08e211a0d36ebc00d945e2a
Jesse Yang <jesseyang432@gmail.com>
2021-07-12 14:04:22 -0500

    Implemented navbar

---
+++ web-server/frontend/src/components/modules/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <div className="NavBar-Container">
+            
+        </div>
+    )
+}
--- web-server/frontend/src/components/pages/SinglePairPredict.js
+++ web-server/frontend/src/components/pages/SinglePairPredict.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import SinglePair from '../modules/SinglePair'
+import NavBar from '../modules/NavBar'
 
 export default function SinglePairPredict() {
     return (
         <div className="SinglePairPredict-Container">
+            <NavBar></NavBar>
             <SinglePair></SinglePair>
         </div>
     )
