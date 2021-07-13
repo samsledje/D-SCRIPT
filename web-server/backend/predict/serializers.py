36a422119abff85253f296c05270a23131d26d16
Jesse Yang <jesseyang432@gmail.com>
2021-07-13 16:02:10 -0500

    Created all pair serializer

--- web-server/backend/predict/serializers.py
+++ web-server/backend/predict/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import SinglePair, ManyPair
+from .models import SinglePair, ManyPair, AllPair
 
 class SinglePairSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +10,8 @@ class ManyPairSerializer(serializers.ModelSerializer):
     class Meta:
         model = ManyPair
         fields = ('id', 'title', 'pairs', 'sequences', 'predictions')
+
+class AllPairSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AllPair
+        fields = ('id', 'title', 'sequences', 'predictions')
