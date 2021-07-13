f732127fe0286424143c138e326aa370913d05c6
Jesse Yang <jesseyang432@gmail.com>
2021-07-13 01:28:11 -0500

    Basic description

--- web-server/frontend/src/components/pages/Home.js
+++ web-server/frontend/src/components/pages/Home.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 import SinglePair from '../modules/SinglePair'
 import ManyPairs from '../modules/ManyPairs'
+import NavBar from '../modules/NavBar'
 
 export default function Home() {
     return (
         <div className="Home-Container">
-            <SinglePair></SinglePair><br></br>
-            <ManyPairs></ManyPairs>
+            <NavBar></NavBar>
+            <div className="Home-Description">
+                <h1>D-SCRIPT</h1>
+                <p>D-SCRIPT (Deep Sequence Contact Residue Interaction Prediction Transfer) is a deep learning method for predicting protein-protein interaction based on the sequences of their amino acids.</p>
+            </div>
         </div>
     )
 }
--- web-server/frontend/src/components/pages/ManyPairsPredict.js
+++ web-server/frontend/src/components/pages/ManyPairsPredict.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import ManyPairs from '../modules/ManyPairs'
+import NavBar from '../modules/NavBar'
 
 export default function ManyPairsPredict() {
     return (
         <div className="ManyPairsPredict-Container">
+            <NavBar></NavBar>
             <ManyPairs></ManyPairs>
         </div>
     )
