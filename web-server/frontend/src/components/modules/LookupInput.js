c4de6aa4b8a80c684665d67cbd4fb1c25ab9ebf0
Jesse Yang <jesseyang432@gmail.com>
2021-07-31 21:41:59 -0400

    Added button

--- web-server/frontend/src/components/modules/LookupInput.js
+++ web-server/frontend/src/components/modules/LookupInput.js
@@ -1,18 +1,26 @@
-import React from 'react'
-
-import { TextField } from '@material-ui/core'
+import React, { useState } from 'react'
+import { TextField, Button } from '@material-ui/core'
+import axios from 'axios'
 
 export default function LookupInput() {
+    const [input, setInput] = useState('');
+
+    const handleInputChange = (e) => {
+        setInput(e.target.value);
+    }
+
     return (
         <div className="LookupInput-Container">
             <TextField
-                    label='Lookup a job by ID'
-                    // value={item.email}
+                    label='Look up a job by ID'
+                    value={input}
                     variant='outlined'
                     margin='dense'
                     fullWidth={true}
-                    required={true}>
-                </TextField>
+                    required={true}
+                    onChange={handleInputChange}>
+            </TextField>
+            <Button variant='contained'>Look up</Button>
         </div>
     )
 }
