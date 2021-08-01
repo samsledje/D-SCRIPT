3ef947660a2596019179121f838e5c8c32a68c7c
Jesse Yang <jesseyang432@gmail.com>
2021-07-31 21:28:28 -0400

    Added Lookup page

---
+++ web-server/frontend/src/components/pages/Lookup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import NavBar from '../modules/NavBar'
+
+export default function Lookup() {
+    return (
+        <div className="Lookup-Container">
+            <NavBar></NavBar>
+        </div>
+    )
+}
