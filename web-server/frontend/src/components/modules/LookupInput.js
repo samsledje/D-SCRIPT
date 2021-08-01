356986790fc81ad0cb1ac22af881c8cfa7ad7220
Jesse Yang <jesseyang432@gmail.com>
2021-07-31 21:56:45 -0400

    Included conditional rendering of lookup result

--- web-server/frontend/src/components/modules/LookupInput.js
+++ web-server/frontend/src/components/modules/LookupInput.js
@@ -2,11 +2,31 @@ import React, { useState } from 'react'
 import { TextField, Button } from '@material-ui/core'
 import axios from 'axios'
 
+import SubmissionModal from './SubmissionModal'
+import LookupResult from './LookupResult'
+
 export default function LookupInput() {
     const [input, setInput] = useState('');
+    const [jobPosition, setJobPosition] = useState('');
+    const [lookupValid, setLookupValid] = useState(false)
 
     const handleInputChange = (e) => {
         setInput(e.target.value);
+    }
+
+    const handleLookup = () => {
+        axios
+            .get(`http://localhost:8000/api/position/${input}/`)
+            .then((res) => {
+                if (res.data.inQueue) {
+                    setLookupValid(true)
+                    setJobPosition(res.data.position)
+                } else {
+                    setLookupValid(false)
+                    setJobPosition('None')
+                }
+            })
+            .catch((err) => console.log(err))
     }
 
     return (
@@ -20,7 +40,8 @@ export default function LookupInput() {
                     required={true}
                     onChange={handleInputChange}>
             </TextField>
-            <Button variant='contained'>Look up</Button>
+            <Button variant='contained' onClick={handleLookup}>Look up</Button>
+            {lookupValid ? <LookupResult position={jobPosition}></LookupResult> : ''}
         </div>
     )
 }
---
+++ web-server/frontend/src/components/modules/LookupResult.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LookupResult(props) {
+    return (
+        <div className="LookupResult-Container">
+            Your job is queued in position {props.position}
+        </div>
+    )
+}
