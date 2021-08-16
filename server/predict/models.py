48225c4a7728c7f689e077c4816f09e8eeca7c2a
Jesse Yang <jesseyang432@gmail.com>
2021-08-16 14:28:20 -0500

    Removed unnecessary predict code

--- server/predict/models.py
+++ server/predict/models.py
@@ -2,61 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class SinglePair(models.Model):
-    title = models.CharField(max_length=120)
-    protein1 = models.CharField(max_length=60)
-    protein2 = models.CharField(max_length=60)
-    sequence1 = models.TextField()
-    sequence2 = models.TextField()
-    probability = models.DecimalField(decimal_places=5, max_digits=6)
-
-    def __str__(self):
-        return self.title
-
-class ManyPair(models.Model):
-    title = models.CharField(max_length=120)
-    pairs = models.FileField(upload_to='pairs/')
-    sequences = models.FileField(upload_to='sequences/')
-    predictions = models.TextField()
-    # predictions = models.FileField(upload_to='predictions/')
-
-    def __str__(self):
-        return self.title
-
-class AllPair(models.Model):
-    title = models.CharField(max_length=120)
-    sequences = models.FileField(upload_to='sequences/')
-    predictions = models.TextField()
-
-    def __str__(self):
-        return self.title
-
-class PairsUpload(models.Model):
-    """
-    Model for uploaded pairs file
-    """
-    pairs = models.FileField(upload_to='pairs/')
-
-class PairsInput(models.Model):
-    """
-    Model for inputted pairs
-    """
-    pairs = models.TextField()
-
-class SeqsUpload(models.Model):
-    """
-    Model for uploaded sequences file
-    """
-    seqs = models.FileField(upload_to='seqs/')
-
-class SeqsInput(models.Model):
-    """
-    Model for inputted sequences
-    """
+class Job(models.Model):
+    uuid = models.UUIDField()
+    title = models.TextField(blank=True)
+    email = models.EmailField()
+    seqsIndex = models.CharField(max_length=1)
+    pairsIndex = models.CharField(max_length=1)
     seqs = models.TextField()
+    pairs = models.TextField(blank=True)
+    completed = models.BooleanField()
 
-class PredictionJob(models.Model):
-    """
-    Class representing a prediction job
-    """
-    email = models.TextField()
+    def __str__(self):
+        return self.uuid
--- server/predict/views.py
+++ server/predict/views.py
@@ -1,18 +1,10 @@
-from django.shortcuts import render
 from django.views.generic import View
 from django.conf import settings
-from django.http import QueryDict, HttpResponse
-from django.views.decorators.csrf import ensure_csrf_cookie
-from rest_framework import viewsets
+from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import pandas as pd
-
-from .serializers import SinglePairSerializer, ManyPairSerializer, AllPairSerializer
-from .serializers import PairsUploadSerializer, PairsInputSerializer, SeqsUploadSerializer, SeqsInputSerializer, PredictionJobSerializer
-from .models import SinglePair, ManyPair, AllPair
-from .models import PairsUpload, PairsInput, SeqsUpload, SeqsInput, PredictionJob
 
 from .api import dscript
 
@@ -67,89 +59,13 @@ class Job():
                 self.seqs += line.decode('utf-8')
         print(self.seqs)
 
-        # print(type(self.seqs))
-        # for line in self.seqs:
-        #     print(line)
-
     def process(self):
-        # print(type(self.seqs))
-        # for line in self.seqs:
-        #     print(line)
         predict_file = dscript.predict(self.pairsIndex, self.seqsIndex, self.pairs, self.seqs, self.id)
-        dscript.email_results(self.email, predict_file, self.id, title=self.title)
+        try:
+            dscript.email_results(self.email, predict_file, self.id, title=self.title)
+        except:
+            print('Not a valid email')
         return predict_file
-    
-
-@api_view(['POST'])
-def test_append(request):
-    test.append(1)
-    return Response(test)
-
-@api_view(['GET', 'POST'])
-def single_pair_predict(request):
-    """
-    List all single pair predictions, or create a new prediction.
-    """
-    if request.method == 'GET':
-        predictions = SinglePair.objects.all()
-        serializer = SinglePairSerializer(predictions, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        data = request.data.copy() #dictionary
-        data['probability'] = dscript.single_pair_predict(data['sequence1'], data['sequence2'])
-        serializer = SinglePairSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            print('NOT A VALID POST')
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'POST'])
-def many_pair_predict(request):
-    """
-    List all many pair predictions, or create a new set of predictions
-    """
-    if request.method == 'GET':
-        predictions = ManyPair.objects.all()
-        serializer = ManyPairSerializer(predictions, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        data = request.data.copy()
-        data['predictions'] = dscript.many_pair_predict(data['title'], data['pairs'], data['sequences'])
-        serializer = ManyPairSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            print('NOT A VALID POST')
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'POST'])
-def all_pair_predict(request):
-    """
-    List all 'all pair' predictions, or create a new set of predictions
-    """
-    if request.method == 'GET':
-        predictions = AllPair.objects.all()
-        serializer = AllPairSerializer(predictions, many=True)
-        return Response(serializer.data)
-    
-    elif request.method == 'POST':
-        data = request.data.copy()
-        data['predictions'] = dscript.all_pair_predict(data['title'], data['sequences'])
-        serializer = AllPairSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            print('NOT A VALID POST')
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-pairsIndexToSerializer = {'1': PairsUploadSerializer, '2': PairsInputSerializer}
-seqsIndexToSerializer = {'1': SeqsUploadSerializer, '2': SeqsInputSerializer}
 
 @api_view(['GET', 'POST'])
 def predict(request):
@@ -159,42 +75,25 @@ def predict(request):
     """
     if request.method == 'POST':
         data = request.data
-        # if data['pairsIndex'] in ['1', '2']:
-        #     pairsSerializer = pairsIndexToSerializer[data['pairsIndex']](data=data['pairs'])
-        #     if pairsSerializer.is_valid():
-        #         pairsSerializer.save()
-        #     else:
-        #         pass
-        # seqsSerializer = seqsIndexToSerializer[data['seqsIndex']](data=data['seqs'])
-        # if seqsSerializer.is_valid():
-        #     seqsSerializer.save()
-        #     print(seqsSerializer)
-        #     print(seqsSerializer.data)
-        #     seqs = seqsSerializer.data['seqs']
-        # else:
-        #     print('not valid seqs serializer')
-        #     pass
         id = uuid.uuid4()
         job = Job(data['pairsIndex'], data['seqsIndex'], data['pairs'], data['seqs'], data['email'], data['title'], id)
-        # predict_file = dscript.predict(data['pairsIndex'], data['seqsIndex'], data['pairs'], data['seqs'], id)
-        # dscript.email_results(data['email'], predict_file, id, title=data['title'])
         jobs.append(job)
         response = {'id': id, 'first': False}
         if len(jobs) == 1:
             response['first'] = True
         return Response(response)
-        # jobs.append(job)
-        # predict_file = dscript.predict(data['pairsIndex'], data['seqsIndex'], data['pairs'], data['seqs'], id)
-        # dscript.email_results(data['email'], predict_file, id, title=data['title'])
-        # return Response(predict_file)
-
-@api_view(['POST'])
-def get_queue_pos(request):
-    id = request.data['id']
-    for i in range(len(jobs)):
-        if str(jobs[i].id) == id:
-            return Response({'position': i+1, 'inQueue': True})
-    return Response({'position': 0, 'inQueue': False})
+        job_data = {'uuid': job.id, 'title': job.title, 'email': job.email, 'seqsIndex': job.seqsIndex, 'pairsIndex': job.pairsIndex, 'seqs': job.seqs, 'pairs': job.pairs, 'completed': False}
+        serializer = JobSerializer(data=job_data)
+        if serializer.is_valid():
+            serializer.save()
+            jobs.append(job)
+            response = {'id': id, 'first': False}
+            if len(jobs) == 1:
+                response['first'] = True
+            return Response(response)
+        else:
+            print(serializer.errors)
+            print('NOT A VALID SERIALIZER')
 
 @api_view(['GET'])
 def get_pos(request, id):
@@ -223,9 +122,3 @@ def run_jobs():
     jobs.pop(0)
     if jobs:
         run_jobs()
-
-
-
-# class PredictionView(viewsets.ModelViewSet):
-#     serializer_class = PredictionSerializer
-#     queryset = Prediction.objects.all()
