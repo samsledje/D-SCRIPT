39fa1517554af50c04a040f43713b74f4ee314ec
Jesse Yang <jesseyang432@gmail.com>
2021-08-16 13:57:49 -0500

    Removed cors whitelist and middleware

--- server/server/settings.py
+++ server/server/settings.py
@@ -54,12 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-]
-
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'http://localhost:3001',
 ]
 
 ROOT_URLCONF = 'server.urls'
