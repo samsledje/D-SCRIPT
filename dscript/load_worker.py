added tests for bipartite prediction and fixed silent failure causing hanging

dscript/load_worker.py
@@ -1,6 +1,6 @@
 import h5py
 import torch
-
+from loguru import logger
 
 # Worker process function for loading embeddings
 def _hdf5_load_partial_func(qin, qout, file_path):
@@ -12,9 +12,12 @@ def _hdf5_load_partial_func(qin, qout, file_path):
     # presumably because the processes can also read in parallel.
     # But, I should investigate whether having a small (but >1) number does best
     torch.set_num_threads(1)
-    with h5py.File(file_path, "r") as fi:
-        for k, i in iter(qin.get, None):
-            emb = torch.from_numpy(fi[k][:])
-            emb.share_memory_()
-            qout.put((i, emb))
-        qout.put(None)
+    try:
+        with h5py.File(file_path, "r") as fi:
+            for k, i in iter(qin.get, None):
+                emb = torch.from_numpy(fi[k][:])
+                emb.share_memory_()
+                qout.put((i, emb))
+            qout.put(None)
+    except Exception as e:
+        logger.error(e)

dscript/loading.py
@@ -1,5 +1,8 @@
+import sys
+import queue
 import torch.multiprocessing as mp
 from tqdm import tqdm
+from loguru import logger
 
 from .load_worker import _hdf5_load_partial_func
 
@@ -7,19 +10,20 @@ from .load_worker import _hdf5_load_partial_func
 # Seperate managing the pool from the loading function
 # to allow the calling process to keep the pool around
 class LoadingPool:
-    def __init__(self, file_path, n_jobs=-1):
+    def __init__(self, file_path, n_jobs=-1, timeout=60):
         if n_jobs < 1:
             self.n_jobs = mp.cpu_count()
         else:
             self.n_jobs = n_jobs
         # Remark: the data loading itself is multi-threaded under the hood
         # So, CPU utilization is high regardless of how many processes are used
-        # But throughbut is a bit faster using multiple processes for some reason
+        # But throughput is a bit faster using multiple processes for some reason
 
         # Note: Using spawn (or torch.mp.spawn) caused errors, make sure to use fork
         ctx = mp.get_context("fork")
         self.input_queue = ctx.Queue()
         self.output_queue = ctx.Queue()
+        self.queue_timeout = timeout
 
         self.pool = ctx.Pool(
             processes=self.n_jobs,
@@ -39,12 +43,16 @@ class LoadingPool:
         if progress:
             pbar = tqdm(total=count, desc="Loading Embeddings")
         while loaded < count:
-            res = self.output_queue.get()
-            i, emb = res
-            embeddings[i] = emb
-            if progress:
-                pbar.update(1)
-            loaded += 1
+            try:
+                res = self.output_queue.get(timeout=self.queue_timeout)
+                i, emb = res
+                embeddings[i] = emb
+                if progress:
+                    pbar.update(1)
+                loaded += 1
+            except queue.Empty:
+                logger.error("Loading embeddings timed out -- see errors above.")
+                sys.exit(7)
         if progress:
             pbar.close()
         return embeddings
@@ -79,6 +87,5 @@ class LoadingPool:
         for _ in range(self.n_jobs):
             self.input_queue.put(None)
         for _ in range(self.n_jobs):
-            res = self.output_queue.get()
-            assert res is None
+            _ = self.output_queue.get(timeout=self.queue_timeout)
         self.pool.join()

dscript/tests/test_commands.py
@@ -1,4 +1,5 @@
 import os
+import h5py
 import shutil
 import subprocess as sp
 import tempfile
@@ -39,14 +40,20 @@ class TestCommands:
     def test_embed(self):
         cmd = f"dscript embed --seqs dscript/tests/test.fasta --outfile {self.temp_dir}/test_embed.h5"
         self._run_command(cmd)
+        assert os.path.exists(f"{self.temp_dir}/test_embed.h5"), "Embedding file not created."
+        with h5py.File(f"{self.temp_dir}/test_embed.h5", "r") as f:
+            klist = list(f.keys())
+            assert len(klist) == 3, "Expected 3 keys in the embedding file."
 
     def test_train_with_topsy_turvy(self):
-        cmd = f"dscript train --topsy-turvy --train dscript/tests/test.tsv --test dscript/tests/test.tsv --embedding {self.temp_dir}/test_embed.h5 --outfile {self.temp_dir}/test_tt-train.log --save-prefix {self.temp_dir}/test_train"
+        cmd = f"dscript train --topsy-turvy --train dscript/tests/test.tsv --test dscript/tests/test.tsv --embedding {self.temp_dir}/test_embed.h5 --outfile {self.temp_dir}/test_tt-train.log --save-prefix {self.temp_dir}/test_tt-train"
         self._run_command(cmd)
+        assert os.path.exists(f"{self.temp_dir}/test_tt-train_final.sav"), "Model file not created."
 
     def test_train_without_topsy_turvy(self):
-        cmd = f"dscript train --train dscript/tests/test.tsv --test dscript/tests/test.tsv --embedding {self.temp_dir}/test_embed.h5 --outfile {self.temp_dir}/test_tt-train.log --save-prefix {self.temp_dir}/test_train"
+        cmd = f"dscript train --train dscript/tests/test.tsv --test dscript/tests/test.tsv --embedding {self.temp_dir}/test_embed.h5 --outfile {self.temp_dir}/test-train.log --save-prefix {self.temp_dir}/test_train"
         self._run_command(cmd)
+        assert os.path.exists(f"{self.temp_dir}/test_train_final.sav"), "Model file not created."
 
     def test_evaluate(self):
         cmd = f"dscript evaluate --test dscript/tests/test.tsv --embeddings {self.temp_dir}/test_embed.h5 --model {self.temp_dir}/test_train_final.sav --outfile {self.temp_dir}/test_evaluate"
@@ -56,6 +63,10 @@ class TestCommands:
         if torch.cuda.is_available():
             cmd = f"dscript predict --pairs dscript/tests/test.tsv --embeddings {self.temp_dir}/test_embed.h5 --model {self.temp_dir}/test_train_final.sav --outfile {self.temp_dir}/test_predict --thresh 0.05 --device 0"
             self._run_command(cmd)
+            with open(f"{self.temp_dir}/test_predict.tsv", "r") as f:
+                lines = f.readlines()
+                assert len(lines) > 0, "Output file is empty."
+                assert len(lines) == 3, "Expected 3 lines in the output file."
         else:
             logger.warning("CUDA is not available, skipping GPU prediction test.")
 
@@ -63,13 +74,37 @@ class TestCommands:
         if torch.cuda.is_available():
             cmd = f"dscript predict --pairs dscript/tests/test.tsv --embeddings {self.temp_dir}/test_embed.h5 --model {self.temp_dir}/test_train_final.sav --outfile {self.temp_dir}/test_predict --thresh 0.05 --device all"
             self._run_command(cmd)
+            with open(f"{self.temp_dir}/test_predict.tsv", "r") as f:
+                lines = f.readlines()
+                assert len(lines) > 0, "Output file is empty."
+                assert len(lines) == 3, "Expected 3 lines in the output file."
         else:
             logger.warning("CUDA is not available, skipping GPU prediction test.")
 
     def test_predict_on_cpu(self):
         cmd = f"dscript predict --pairs dscript/tests/test.tsv --embeddings {self.temp_dir}/test_embed.h5 --model {self.temp_dir}/test_train_final.sav --outfile {self.temp_dir}/test_predict --thresh 0.05 --device cpu"
         self._run_command(cmd)
+        with open(f"{self.temp_dir}/test_predict.tsv", "r") as f:
+                lines = f.readlines()
+                assert len(lines) > 0, "Output file is empty."
+                assert len(lines) == 3, "Expected 3 lines in the output file."
 
-    def test_serial(self):
+    def test_predict_bipartite(self):
+        cmd = f"dscript predict_bipartite --protA dscript/tests/test.bipartite.txt --protB dscript/tests/test.bipartite.txt --embedA {self.temp_dir}/test_embed.h5 --model {self.temp_dir}/test_train_final.sav --outfile {self.temp_dir}/test_bipartite --thresh 0.05 --device 0"
+        self._run_command(cmd)
+        with open(f"{self.temp_dir}/test_bipartite.tsv", "r") as f:
+            lines = f.readlines()
+            assert len(lines) > 0, "Output file is empty."
+            assert len(lines) == 9, "Expected 9 lines in the output file." 
+
+    def test_predict_bipartite_cpu(self):
+        cmd = f"dscript predict_bipartite --protA dscript/tests/test.bipartite.txt --protB dscript/tests/test.bipartite.txt --embedA {self.temp_dir}/test_embed.h5 --model {self.temp_dir}/test_train_final.sav --outfile {self.temp_dir}/test_bipartite_cpu --thresh 0.05 --device cpu"
+        self._run_command(cmd)
+        with open(f"{self.temp_dir}/test_bipartite_cpu.tsv", "r") as f:
+            lines = f.readlines()
+            assert len(lines) > 0, "Output file is empty."
+            assert len(lines) == 9, "Expected 9 lines in the output file."
+
+    def test_predict_serial(self):
         cmd = f"dscript predict_serial --pairs dscript/tests/test.tsv --embeddings {self.temp_dir}/test_embed.h5 --model {self.temp_dir}/test_train_final.sav --outfile {self.temp_dir}/test_serial --thresh 0.05"
         self._run_command(cmd)